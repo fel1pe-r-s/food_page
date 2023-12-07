fa7c1086f7bf18a17b3295b09fb211f4ec790fe9
fel1pe-r-s <1felipersilva1@gmail.com>
2023-12-07 08:20:45 -0300

    SignUp

--- web/src/pages/SignUp/index.jsx
+++ web/src/pages/SignUp/index.jsx
@@ -4,15 +4,22 @@ import Button from "../../components/Button";
 import ButtonText from "../../components/ButtonText";
 
 import logo from "../../assets/icons/logo.svg";
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
       <Logo>
         <img src={logo} alt="logo food page" />
       </Logo>
       <Content>
-        <h2>Faça login</h2>
+        <h2>Crie sua Conta</h2>
         <form>
+          <Input
+            typeInput="name"
+            label="Nome"
+            id="name"
+            type="text"
+            placeholder="Exemplo: Maria da Silva"
+          />
           <Input
             typeInput="email"
             label="Email"
@@ -27,9 +34,16 @@ export function SignIn() {
             type="password"
             placeholder="No mínimo 6 caracteres"
           />
-          <Button title="Entrar" />
+          <Input
+            typeInput="confirmPassword"
+            label="Confirme a senha"
+            id="confirmPassword"
+            type="password"
+            placeholder="No mínimo 6 caracteres"
+          />
+          <Button title="Criar Conta" />
         </form>
-        <ButtonText to="/register" title="Criar uma conta" />
+        <ButtonText to="/" title="Já tenho uma conta" />
       </Content>
     </Container>
   );
