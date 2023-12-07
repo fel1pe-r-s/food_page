de7fd8c2ca8f332cd8d7be982c314f10e4354047
fel1pe-r-s <1felipersilva1@gmail.com>
2023-12-07 02:22:53 -0300

    Restarting Project

--- web/src/components/Slide/index.jsx
+++
@@ -1,5 +0,0 @@
-import "./index.css";
-
-export function Slide({ children }) {
-  return <div className="slide">{children}</div>;
-}
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Home } from "./pages/home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <h1>ola</h1>
   </React.StrictMode>
 );
